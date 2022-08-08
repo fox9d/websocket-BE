0fdc08a7e412ac0fa3b8c2473434f20f9078206d
ju-ei8ht <qkr8125@gmail.com>
2022-08-08 18:03:25 +0900

    log 추가

--- src/main/java/com/example/websocket/controller/MessageController.java
+++ src/main/java/com/example/websocket/controller/MessageController.java
@@ -3,16 +3,19 @@ package com.example.websocket.controller;
 import com.example.websocket.model.Message;
 import com.example.websocket.model.dto.MessageRequestDto;
 import com.example.websocket.model.dto.MessageResponseDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class MessageController {
     @MessageMapping("/receive")
     @SendTo("/sub/chat")
     public MessageResponseDto messaging(MessageRequestDto requestDto){
         Message message = new Message(requestDto);
+        log.info("payload: "+message.getContent());
         return new MessageResponseDto(message);
     }
 }
